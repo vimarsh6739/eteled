5ac3ff7d153b2754ef621a0c5dc02aee1fffd2f4
Vimarsh Sathia <vimarsh.sathia@gmail.com>
2024-02-23 21:32:58 -0600

    Added cpp

---
+++ delete.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+#include <cstdlib> 
+
+int main(int argc, char* argv[]){
+  
+  std::cout << "Hey there:  isn't it a bit empty in this folder?" << std::endl;
+  std::system("rm -f a.out");
+  std::system("rm -f delete.cpp");
+  return 0;
+}
